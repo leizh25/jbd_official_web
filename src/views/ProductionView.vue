fc944a6661bda3867185075ba9203d8824e67911
XIONG <1639372106@qq.com>
2024-05-03 20:36:56 +0800

    feat: 添加

--- src/views/ProductionView.vue
+++ src/views/ProductionView.vue
@@ -1,6 +1,472 @@
 <template>
-  <div>产品中心</div>
-  <router-link to="/production/detail">产品详情</router-link>
+  <main class="container">
+    <section class="nav">
+      <div class="desc">
+        <div style="margin: 0 auto">
+          <div class="desc1">
+            <span>产品中心</span>
+          </div>
+          <div class="desc2">
+            <span>专注于锂电池保护解决方案</span>
+          </div>
+        </div>
+      </div>
+      <div class="localtion">
+        <div>
+          <i>标</i>
+          <span>首页 - 产品中心 -详情</span>
+        </div>
+      </div>
+    </section>
+
+    <section class="content">
+      <div class="top">
+        <div class="top1">
+          <span>产品中心</span>
+          <div></div>
+        </div>
+        <div class="top2">
+          <span>智能·只为锂电安全</span>
+        </div>
+      </div>
+
+      <div class="main">
+        <nav class="sidebar">
+          <ul>
+            <span>
+              智能锂电池保护板
+              <i>标</i>
+            </span>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+          </ul>
+          <ul>
+            <span>
+              智能锂电池保护板
+              <i>标</i>
+            </span>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+          </ul>
+          <ul>
+            <span>
+              智能锂电池保护板
+              <i>标</i>
+            </span>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+          </ul>
+          <ul>
+            <span>
+              智能锂电池保护板
+              <i>标</i>
+            </span>
+            <li><a href="#">带通讯5串以下保护板</a></li>
+          </ul>
+          <ul>
+            <span>
+              智能锂电池保护板
+              <i>标</i>
+            </span>
+          </ul>
+        </nav>
+        <section class="right">
+          <ul>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+            <li>
+              <div><img src="./images/product_center.jpg" alt="这是一张图片" width="232px" height="200px" /></div>
+              <span>12V锂电池保护板3/4串三元铁锂户外电源大电流250A300A蓝牙</span>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </section>
+  </main>
 </template>
-<script setup lang="ts"></script>
-<style></style>
+
+<style scoped>
+body,
+ol,
+ul,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+th,
+td,
+dl,
+dd,
+form,
+fieldset,
+legend,
+input,
+textarea,
+select,
+figure,
+figcaption {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+b,
+strong {
+  font-size: 100%;
+  font-weight: normal;
+}
+
+i,
+em {
+  font-style: normal;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  color: #333;
+  text-decoration: none;
+}
+
+body,
+html {
+  font:
+    14px '微软雅黑',
+    Arial;
+  color: #333;
+}
+
+input {
+  outline: none;
+}
+
+.clearfix {
+  zoom: 1;
+}
+
+.clearfix:after {
+  content: '';
+  display: block;
+  height: 0;
+  overflow: hidden;
+  clear: both;
+}
+
+.container {
+  width: 100%;
+  overflow: hidden;
+}
+
+.nav {
+  height: 438px;
+}
+
+.desc {
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 370px;
+  opacity: 0.2;
+  background-color: aqua;
+}
+
+.desc .desc1 {
+  /* 产品中心 */
+  position: absolute;
+  left: 45%;
+  top: 177px;
+  width: 240px;
+  height: 66px;
+  opacity: 1;
+  font-family: Source Han Sans;
+  font-size: 60px;
+  font-weight: bold;
+  line-height: 66px;
+  letter-spacing: 0px;
+  font-variation-settings: 'opsz' auto;
+  font-feature-settings: 'kern' on;
+  color: #ffffff;
+}
+
+.desc .desc2 {
+  /* 专注于锂电池保护解决方案 */
+  position: absolute;
+  left: 45%;
+  top: 270px;
+  width: 384px;
+  height: 66px;
+  opacity: 1;
+  font-family: Source Han Sans;
+  font-size: 32px;
+  font-weight: 500;
+  line-height: 66px;
+  letter-spacing: 0px;
+  font-variation-settings: 'opsz' auto;
+  font-feature-settings: 'kern' on;
+  color: #ffffff;
+}
+
+.localtion {
+  left: 0px;
+  width: 100%;
+  height: 68px;
+  opacity: 1;
+  background: #f5f8fa;
+  line-height: 68px;
+}
+
+.localtion > div {
+  position: absolute;
+  left: 320px;
+}
+
+.localtion > div > i {
+  width: 22px;
+  height: 22px;
+  opacity: 1;
+}
+
+.localtion > div > span {
+  margin-left: 10.83px;
+  width: 150px;
+  height: 22px;
+  width: 150px;
+  height: 22px;
+  opacity: 1;
+  font-family: 思源黑体;
+  font-size: 16px;
+  font-weight: normal;
+  line-height: 22px;
+  letter-spacing: 0px;
+  font-variation-settings: 'opsz' auto;
+  color: #555555;
+}
+
+.content {
+  width: 1920px;
+  background-color: skyblue;
+  margin: 0 auto;
+}
+
+.content > .top {
+  width: 1920px;
+  height: 105px;
+}
+
+.content > .top > .top1 {
+  /* 产品中心 */
+  /* 组 133 */
+  float: left;
+  left: 320px;
+  margin-top: 50px;
+  width: 120px;
+  height: 47px;
+  opacity: 1;
+  line-height: 47px;
+}
+
+.content > .top > .top1 > span {
+  left: 0px;
+  top: 0px;
+  width: 120px;
+  height: 34px;
+  opacity: 1;
+  font-family: Source Han Sans;
+  font-size: 30px;
+  font-weight: bold;
+  line-height: 34px;
+  letter-spacing: 0px;
+  font-variation-settings: 'opsz' auto;
+  font-feature-settings: 'kern' on;
+  color: #333333;
+}
+
+.content > .top > .top1 > div {
+  left: 0px;
+  top: 43px;
+  width: 42px;
+  height: 4px;
+  opacity: 1;
+  background: #333333;
+  margin-top: 1.5px;
+}
+
+.content > .top > .top2 {
+  float: right;
+  width: 180px;
+  height: 29px;
+  margin: 59px 295px 0px 0px;
+  opacity: 1;
+  font-family: 思源黑体;
+  font-size: 20px;
+  font-weight: normal;
+  line-height: 28.08px;
+  letter-spacing: 0px;
+  color: #555555;
+  text-align: center;
+
+  margin-top: 4%;
+}
+
+.content > .main {
+  display: flex;
+  height: 200px;
+  margin-top: 50px;
+}
+
+.content > .main > .sidebar {
+  width: 300px;
+}
+
+.content > .main > .sidebar > ul {
+  display: block;
+  line-height: 50px;
+  border-radius: 6px;
+  opacity: 1;
+  text-align: center;
+}
+
+.content > .main > .sidebar > ul > span {
+  display: block;
+  font-family: Source Han Sans;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 50px;
+  letter-spacing: 0px;
+  font-variation-settings: 'opsz' auto;
+  color: #ffffff;
+  background-color: #333;
+}
+
+.content > .main > .sidebar > ul > span > i {
+  float: right;
+  left: 7.55px;
+  top: 1px;
+  height: 87.5%;
+  margin-right: 43px;
+  opacity: 1;
+}
+
+.content > .main > .sidebar > ul > li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 23px;
+  width: 154px;
+  opacity: 1;
+  font-family: Source Han Sans;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 23px;
+  letter-spacing: 0px;
+  font-variation-settings: 'opsz' auto;
+  color: #ffffff !important;
+  margin-top: 20px;
+  margin-left: 28%;
+  text-align: center;
+}
+
+.content > .main > .sidebar > ul > li:first-child {
+  margin-top: 0px !important;
+}
+
+.content > .main > .sidebar > ul > li:last-child {
+  margin-bottom: 20px !important;
+}
+
+.content > .main > .sidebar > ul > li > a {
+  color: #333333;
+}
+
+.content > .main > .right {
+  display: flex;
+  width: 1060px !important;
+  height: 300px;
+  background-color: aqua;
+  margin-left: 20px;
+  margin-right: 295px;
+}
+
+.content > .main > .right > ul {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.content > .main > .right > ul > li {
+  display: block;
+  width: 320px;
+  height: 320px;
+  text-align: center;
+  border-radius: 6px;
+  opacity: 1;
+  box-sizing: border-box;
+  border: 1px solid #d6d6d6;
+  background-color: antiquewhite;
+}
+
+.content > .main > .right > ul > li > div > img {
+  background-color: aquamarine;
+}
+
+.content > .main > .right > ul > li > span {
+  display: block;
+  width: 190px;
+  height: 36px;
+  opacity: 1;
+  margin-left: 65px;
+  margin-top: 19.94px;
+  font-family: Source Han Sans;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 18px;
+  letter-spacing: 0px;
+
+  font-variation-settings: 'opsz' auto;
+  font-feature-settings: 'kern' on;
+  color: #3d3d3d;
+}
+</style>
