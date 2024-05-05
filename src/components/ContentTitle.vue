12c14ad5df3b0086602d352716f3803969c2d7b1
leizhuzheng <leizhuzheng@qq.com>
2024-05-05 18:11:46 +0800

    feat: 关于我们

---
+++ src/components/ContentTitle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="title_box">
+    <div class="title">{{ title }}</div>
+  </div>
+</template>
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    title: string;
+  }>(),
+  {
+    title: '我是标题'
+  }
+);
+</script>
+<style lang="scss" scoped>
+.title_box {
+  height: 105px;
+  padding-top: 60px;
+  box-sizing: border-box;
+  // background-color: aqua;
+  .title {
+    font-family: 'Source Han Sans';
+    font-size: 30px;
+    font-weight: bold;
+    color: #333;
+    position: relative;
+    &::before {
+      content: '';
+      display: block;
+      width: 42px;
+      height: 4px;
+      background-color: #333;
+      position: absolute;
+      bottom: -15px;
+    }
+  }
+}
+</style>
